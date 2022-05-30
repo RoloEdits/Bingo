48ed7091ca753d136f436a68528a20a1d27fb582
RoloEdits <RoloEdits@gmail.com>
2022-05-30 14:18:44 -0700

    removed set from Player class

--- Player.cs
+++ Player.cs
@@ -2,9 +2,9 @@
 {
     internal class Player
     {
-        public string Name { get; set; }
-        public string Guess { get; set; }
-        public int Score { get; set; }
+        public string Name { get; }
+        public string Guess { get; }
+        public int Score { get; }
 
         public Player(string name, string guess, int score)
         {
