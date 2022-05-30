e498d66264368a887c5f95258c84ade80ce30386
RoloEdits <RoloEdits@gmail.com>
2022-05-30 14:17:05 -0700

    Create Utilities.cs
    created utilities class to hold ascii art and  string format methods

---
+++ Utilities.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tog.Bingo
+{
+    public static class Utilities
+    {
+        // Formats string inputs by removing spaces, new lines, returns, and makes all characters uppercase.
+        public static string StringFormat(string? formatedString)
+        {
+            // Redundant NULL input handling to allow graceful exit.
+            if (formatedString == null)
+            {
+                Console.Clear();
+                AsciiTitle();
+                Console.WriteLine("Error: Invalid Input. Press any key to exit program...");
+                Console.ReadKey();
+                Console.ResetColor();
+                Environment.Exit(1);
+            }
+            return formatedString.Replace(" ", "").Replace("\r\n", "").Replace("\n", "").ToUpper();
+        }
+
+        // Holder for the Ascii Art Title
+        public static void AsciiTitle()
+        {
+            Console.WriteLine(@"                                                           .,.                                      
+                                                        .,*****,,.                                  
+                                                    .,,,.........,,,,.                              
+                                                ..,,,..,,,*****,,...,,,,.                           
+                                              .,,,..,,************,,...,,,.                         
+                                            .,,,...,,******,,,*****,,...,,*,.                       
+                                         .,,,......,,****,,...,,****,....,,**,,.                    
+                                      .,,,***,,....,,*******,,*****,,...,,***,,,...                 
+                                 .,,**,,,,,,,**,,...,,************,,..,,***,,,,,,,,**,,.            
+                              .,*,,............,,,*,,..,,******,,...,,*,,............,,*,,          
+                           .,*,,....,,*******,,....,,**,,,.....,,,**,,...,,,******,,...,,**.        
+                          ,,,....,,************,,....,,***********,,...,,************,,...,,,.      
+                         *,,.....,****,,..,,****,,.....,,**,,,,,*,,....,****,,...,,***,....,**   
+                          ,,,...,,****,,..,,****,,....,,,,.     .,,,...,*****,..,,****,..,,,,       
+                           .,,,..,,*************,...,,*,.         .,,,.,,************,,,,*,,       
+                             .,,,,.,,*********,,..,,*,.             ..,,..,,******,,.,,*,.          
+                              .,,,,,,..........,,*,..                  .,*,,.....,,,*,,..         
+                                    .,,,**,,,,..                          ..,,,,*,..                
+                                                                                                    
+                                                .,.        .,.       .,.                                     
+                                               .,,,.      .,,,.     .,,,.                            
+                                                .,.        .,.       .,.");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
