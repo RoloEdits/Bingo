99785165af7b311a7da797492ba5053c81366131
Rolo <roloedits@gmail.com>
2022-09-03 22:46:00 -0700

    Changed Config to Format

---
+++ Bingo/Format.cs
@@ -0,0 +1,29 @@
+﻿namespace Bingo;
+
+public record Format
+{
+    public byte Columns { get; init; }
+    public byte Rows { get; init; }
+    public byte BaseSquareValue { get; init; }
+    public byte RowValueOffset { get; init; }
+    public byte BonusColumns { get; init; }
+    public byte BonusMultiplier { get; init; }
+    public char BonusSkipChar { get; init; }
+
+    public Format()
+    {
+
+    }
+
+    public Format(byte columns, byte rows, byte baseSqaurevalue, byte rowOffsetValue, byte bonusColumns, byte bonusMultiplier)
+    {
+        Columns = columns;
+        Rows = rows;
+        BaseSquareValue = baseSqaurevalue;
+        RowValueOffset = rowOffsetValue;
+        BonusColumns = bonusColumns;
+        BonusMultiplier = bonusMultiplier;
+
+        BonusSkipChar = 'P';
+    }
+}
