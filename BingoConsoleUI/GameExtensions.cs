fda49ccfd030c4cd3e4b86f85ab5799c6d1e9301
Rolo <roloedits@gmail.com>
2022-09-08 18:42:09 -0700

    fixed typos

--- BingoConsoleUI/GameExtensions.cs
+++ BingoConsoleUI/GameExtensions.cs
@@ -1,16 +1,16 @@
 ﻿using Bingo;
 
 namespace BingoConsoleUI;
-internal static class GameExtentions
+internal static class GameExtensions
 {
     public static void End(this Game game)
     {
         Console.Clear();
         Ascii.Title();
-        Console.WriteLine($"Successfully Finished Going Through {game.Players.Count.ToString()} Players' Guesses in {game.ScoreCalculationTime.ToString()}ms.");
+        Console.WriteLine($"Successfully finished scoring {game.Format.TotalSquares} Squares for {game.Players.Count} Players' in {game.ScoreCalculationTime} milliseconds.");
         Console.Write("Press any key to exit...");
         Console.ReadKey(true);
-        Console.WriteLine(Environment.NewLine);
+        Console.Write(Environment.NewLine);
         Console.ResetColor();
         Environment.Exit(0);
     }
