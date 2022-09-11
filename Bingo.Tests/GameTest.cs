c68fb62438fc76465e2f283a8b1547be4b6c101f
Rolo <roloedits@gmail.com>
2022-09-11 09:20:47 -0700

    added unit testing

---
+++ Bingo.Tests/GameTest.cs
@@ -0,0 +1,35 @@
+using Bingo.Library;
+
+namespace Bingo.Tests;
+
+public class GameTest
+{
+
+    [Fact]
+    public void CalculatePlayerScoreTest_Max450()
+    {
+        const string key = "YYYYYYYYYYYY";
+        const string guess = "YYYYYYYYYYYY";
+
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var game = new Game(key,card );
+
+        var result = game.CalculatePlayerScore(guess, true);
+
+        Assert.Equal(450, result);
+    }
+
+    [Fact]
+    public void CalculatePlayerScoreTest_Min450()
+    {
+        const string key = "YYYYYYYYYYYY";
+        const string guess = "NNNNNNNNNNNN";
+
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var game = new Game(key,card);
+
+        var result = game.CalculatePlayerScore( guess, true );
+
+        Assert.Equal(-450, result);
+    }
+}
--- Bingo.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Bingo.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
