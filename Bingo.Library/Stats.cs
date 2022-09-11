91e23a40480fedb8cd2b45f3b8fe711e2805c057
Rolo <roloedits@gmail.com>
2022-09-11 09:24:16 -0700

    broke out stat tracking into own class out of Game

---
+++ Bingo.Library/Stats.cs
@@ -0,0 +1,28 @@
+﻿namespace Bingo.Library;
+
+public class Stats
+{
+    public double ScoreCalculationTime { get; set; }
+    public IDictionary<int, uint> CorrectGuessesPerSquare { get; set; }
+    public List<double> CorrectGuessesPerSquarePercentage { get; set; }
+    public int PlayerCount { get; set; }
+
+    public void GetCorrectGuessesPerSquarePercentage()
+    {
+        foreach (var guess in CorrectGuessesPerSquare)
+        {
+            var (_, count) = guess;
+
+            if (count == 0)
+            {
+                CorrectGuessesPerSquarePercentage.Add(0.0);
+            }
+            else
+            {
+                var percentage = ((double)count / PlayerCount);
+
+                CorrectGuessesPerSquarePercentage.Add(percentage);
+            }
+        }
+    }
+}
