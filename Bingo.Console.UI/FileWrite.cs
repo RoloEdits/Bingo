81bc70b375fe69b2bd8dcc4f446a2e70c0037692
Rolo <roloedits@gmail.com>
2022-09-12 21:37:55 -0700

    added null checks so warning go away

--- Bingo.Console.UI/FileWrite.cs
+++ Bingo.Console.UI/FileWrite.cs
@@ -10,30 +10,32 @@ internal static class FileWrite
         var fileName = GetFileName(Game.Path!);
 
         using TextWriter writer = new StreamWriter(fileName);
-        var playersOrdered = game.Players
-            .OrderByDescending(player => player.Score)
-            .ThenBy(player => player.Name).ToList();
+        var playersOrdered = game?.Players
+            ?.OrderByDescending(player => player.Score)
+            ?.ThenBy(player => player.Name).ToList();
 
-        var table = new Table(game.Card.Columns, game.Card.Rows, game.Card.BonusColumns);
-
-        var percentages = game.Stats.CorrectGuessesPerSquareAsPercentageString;
-        var key = game.Key.ToList();
-
-        writer.Write(table.CreateDynamic("Key", key));
-        writer.WriteLine();
-
-        if (percentages.Count > 0)
+        if (game is not null && playersOrdered is not null)
         {
-            writer.Write(table.CreateDynamic("Stats", percentages));
+            var table = new Table(game.Card.Columns, game.Card.Rows, game.Card.BonusColumns);
+
+            var key = game.Key.ToList();
+            writer.Write(table.CreateDynamic("Key", key));
             writer.WriteLine();
-        }
 
-        writer.WriteLine("| Names | Scores |");
-        writer.WriteLine("|---|---|");
-        foreach (var player in playersOrdered)
-        {
-            // Replaces all pipes with an escaped version so that the Markdown Table wont be broken.
-            writer.WriteLine($"| {player.Name.Replace("|", "\\|")} | {player.Score} |");
+            if (game.WillLogStats)
+            {
+                var percentages = game.Stats.CorrectGuessesPerSquareAsPercentageString;
+                writer.Write(table.CreateDynamic("Stats", percentages));
+                writer.WriteLine();
+            }
+
+            writer.WriteLine("| Names | Scores |");
+            writer.WriteLine("|---|---|");
+            foreach (var player in playersOrdered)
+            {
+                // Replaces all pipes with an escaped version so that the Markdown Table wont be broken.
+                writer.WriteLine($"| {player.Name.Replace("|", "\\|")} | {player.Score} |");
+            }
         }
     }
 
