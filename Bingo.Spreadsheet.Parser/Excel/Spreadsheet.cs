931897a04c209f7975a739cf4b4971fad26963af
Rolo <roloedits@gmail.com>
2022-09-25 13:12:08 -0700

    created spreadsheet parser project

---
+++ Bingo.Spreadsheet.Parser/Excel/Spreadsheet.cs
@@ -0,0 +1,56 @@
+﻿using ClosedXML.Excel;
+using Bingo.Domain.Errors;
+using Bingo.Domain;
+
+namespace Bingo.Spreadsheet.Parser.Excel;
+
+public static class Spreadsheet
+{
+    public static Dictionary<string, string> Parse(string path)
+    {
+        // TODO - Error handling for if the file is already open. Will hold on error and  prompt to hit a key when the file is closed.
+
+        try
+        {
+            using var wb = new XLWorkbook(path);
+
+        }
+        catch (Exception)
+        {
+            throw new CannotOpenFileException("Couldn't open file! Make sure it's not open by another program.");
+        }
+
+        using var workbook = new XLWorkbook(path);
+        var worksheet = workbook.Worksheet(1);
+
+        var playerCount = 0;
+        var row = 1;
+        while (!worksheet.Cell(row, 1).IsEmpty())
+        {
+            playerCount++;
+            row++;
+        }
+
+        if (playerCount == 0)
+        {
+            throw new NoPlayersException("No players found!");
+        }
+
+        var players = new Dictionary<string, string>(playerCount);
+
+        short currentRow = 1;
+        while (!worksheet.Cell(currentRow, 1).IsEmpty())
+        {
+            var name = worksheet.Cell(currentRow, 1).GetString().Trim();
+            var guess = worksheet.Cell(currentRow, 2).GetString().StringFormat();
+
+            if (!players.TryAdd(name, guess))
+            {
+                throw new GuessedMoreThanOnceException("Players can only guess once!", name);
+            }
+
+            currentRow++;
+        }
+        return players;
+    }
+}
