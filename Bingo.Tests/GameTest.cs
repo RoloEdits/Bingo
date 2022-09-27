132b514c92cb54f371425f78de2ba0937af46957
Rolo <roloedits@gmail.com>
2022-09-27 07:32:30 -0700

    renamed library test and tweaked the unit test

--- Bingo.Tests/GameTest.cs
+++ Bingo.Tests/GameTest.cs
@@ -1,6 +1,4 @@
-using Bingo.Library;
-
-namespace Bingo.Tests;
+namespace Bingo.Library.Tests;
 
 public sealed class GameTest
 {
@@ -11,9 +9,17 @@ public sealed class GameTest
         const string guess = "YYYYYYYYYYYY";
 
         var card = new Card(4, 3, 10, 20, 1, 2);
-        var game = new Game(key, card);
+        var settings = new Settings()
+        {
+            WillLogStats = true,
+            WillCountAllSameGuessersInStats = true
+        };
 
-        var result = game.CalculatePlayerScore(guess, true);
+        var player = new Dictionary<string, string>() { { "Rolo", guess } };
+
+        var game = new Game(key, card, settings, player);
+
+        var result = game.CalculatePlayerScore(game.Players[0]);
 
         Assert.Equal(450, result);
     }
@@ -25,9 +31,17 @@ public sealed class GameTest
         const string guess = "NNNNNNNNNNNN";
 
         var card = new Card(4, 3, 10, 20, 1, 2);
-        var game = new Game(key, card);
+        var settings = new Settings()
+        {
+            WillLogStats = true,
+            WillCountAllSameGuessersInStats = true
+        };
 
-        var result = game.CalculatePlayerScore(guess, true);
+        var player = new Dictionary<string, string>() { { "Rolo", guess } };
+
+        var game = new Game(key, card, settings, player);
+
+        var result = game.CalculatePlayerScore(game.Players[0]);
 
         Assert.Equal(-450, result);
     }
