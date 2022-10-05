4202734b74bb508a9746ff7c87d380cc9cfe7183
Rolo <roloedits@gmail.com>
2022-10-05 10:26:13 -0700

    added test for skipping squares

--- Bingo.Tests/GameTest.cs
+++ Bingo.Tests/GameTest.cs
@@ -49,4 +49,28 @@ public sealed class GameTest
 
         Assert.Equal(-450, result);
     }
+
+    [Fact]
+    public void CalculatePlayerScoreTest_ShouldEqual10_WhenOnlyFreeSpaceIsGiven()
+    {
+        const string key = "YYYYYYYYYYYYYYYYYYYYYYYYY";
+        const string guess = "PPPPPPPPPPPPYPPPPPPPPPPPP";
+
+        var card = new Card(5, 5, 10, 0, 5, 1);
+        var settings = new Settings()
+        {
+            WillLogStats = true,
+            WillCountAllSameGuessersInStats = true
+        };
+
+        var player = new Dictionary<string, string>() { { "Rolo", guess } };
+
+        var game = new Game(key, card, settings, player);
+
+        game.CalculateScore(game.Players[0]);
+
+        var result = game.Players[0].Score;
+
+        Assert.Equal(10, result);
+    }
 }
