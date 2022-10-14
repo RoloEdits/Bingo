8ab195ad5d55a555a3d93af4fd20093b507338b2
Rolo <roloedits@gmail.com>
2022-10-13 21:53:08 -0700

    renamed InvalidUserGuessAmount to be more generic so it works for Key

---
+++ Bingo.Domain/Errors/InvalidSquareAmountException.cs
@@ -0,0 +1,14 @@
+﻿using Bingo.Domain.Models;
+
+namespace Bingo.Domain.Errors;
+
+[Serializable]
+public class InvalidSquareAmountException : Exception
+{
+    public InvalidSquareAmountException() { }
+
+    public InvalidSquareAmountException(string message) : base(message) { }
+
+    public InvalidSquareAmountException(string message, Exception inner) : base(message, inner) { }
+
+}
