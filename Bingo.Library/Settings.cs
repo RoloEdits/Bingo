c142ddd8c3e19415eeb690d3dafdea0d5a4fefb0
Rolo <roloedits@gmail.com>
2022-10-13 21:43:03 -0700

    added default false value for WillCountAllSameGuessersInStats

--- Bingo.Library/Settings.cs
+++ Bingo.Library/Settings.cs
@@ -5,5 +5,5 @@ namespace Bingo.Library;
 public class Settings : ISettings
 {
     public bool WillLogStats { get; set; } = true;
-    public bool WillCountAllSameGuessersInStats { get; set; }
+    public bool WillCountAllSameGuessersInStats { get; set; } = false;
 }
