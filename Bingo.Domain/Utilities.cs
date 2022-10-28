60b1814e7ee31b64b3e02779883d1380da00dca5
Rolo <roloedits@gmail.com>
2022-10-28 10:30:06 -0700

    added custom Zip methods to mimic rusts

--- Bingo.Domain/Utilities.cs
+++ Bingo.Domain/Utilities.cs
@@ -66,4 +66,41 @@ public static class Utilities
 
         return array;
     }
+
+    public static IEnumerable<(T, U, int)> Zip<T, U>(this IList<T> tList, IList<U> uList) {
+
+        var smallest = Math.Min(tList.Count, uList.Count);
+
+        for(var i = 0; i < smallest; i++ ) {
+
+            yield return (tList[i], uList[i], i);
+
+        }
+    }
+
+    /// <summary>
+    /// Creates an iterator over two collections allowing to foreach over multiple collections at once.
+    /// </summary>
+    /// <param name="tArray"></param>
+    /// <param name="uArray"></param>
+    /// <typeparam name="T"> is a 2D array</typeparam>
+    /// <typeparam name="U"> is a 2D array</typeparam>
+    /// <returns> A sequence of tuples containing the elements in both collections at the current position of the enumerator and the current index.</returns>
+    public static IEnumerable<(T, U, (int, int))> Zip<T, U>(this T[,] tArray, U[,] uArray) {
+
+        var rows = Math.Min(tArray.GetUpperBound(0), uArray.GetUpperBound(0));
+
+        var columns = Math.Min(tArray.GetUpperBound(1), uArray.GetUpperBound(1));
+
+        for(var row = 0; row < rows; row++ ) {
+
+            for(var column = 0; column < columns; column++ ) {
+
+                yield return (tArray[row, column], uArray[row, column], (row, column));
+
+            }
+
+        }
+
+    }
 }
