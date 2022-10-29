fb4ed80512cf672504eb9e7589d20b728879b63c
Rolo <roloedits@gmail.com>
2022-10-28 21:06:16 -0700

    created Markdown testing project

---
+++ Bingo.Markdown.Tests/TableTest.cs
@@ -0,0 +1,25 @@
+using Bingo.Domain;
+
+namespace Bingo.Markdown.Tests;
+
+public class TableTest
+{
+    [Fact]
+    public void TableShouldMatch()
+    {
+        // Arrange
+        var table = new Table(3, 3, 0);
+        var list = new List<char>(){ 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y' }.ListTo2DArray(3, 3);
+        const string expected = @"|  | 1 | 2 | 3 |
+ :---: | :---: | :---: | :---: |
+| **A** | Y | Y | Y |
+| **B** | Y | Y | Y |
+| **C** | Y | Y | Y |
+";
+        // Act
+        var construct = table.Create<char>("", list);
+
+        // Assert
+        Assert.Equal(expected, construct);
+    }
+}
