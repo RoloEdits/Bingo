0cc9d345850bee291e89c224b9c7ae96a3c89e0b
Rolo <roloedits@gmail.com>
2022-10-28 21:07:03 -0700

    formatted

--- Bingo.Core/Card.cs
+++ Bingo.Core/Card.cs
@@ -17,15 +17,15 @@ public sealed class Card : ICard
     public char[] RowLabels { get; }
     public Square[,] SquareLabels { get; }
 
-    public Card(byte columns, byte rows, byte baseSquareValue, int rowOffsetValue, byte bonusColumns, byte bonusMultiplier, char bonusSkipChar = 'P')
+    public Card(byte columns, byte rows, byte baseSquareValue, int rowOffsetValue, byte bonusColumns,
+        byte bonusMultiplier = 1, char bonusSkipChar = 'P')
     {
-        // TODO: Add defaults so that when there is no bonus there is still the option to skip squares. Just that when there is a bonus, you can only skip on those squares.
         Columns = columns;
         Rows = rows;
         BaseSquareValue = baseSquareValue;
         RowValueOffset = rowOffsetValue;
         BonusColumns = bonusColumns;
-        BonusMultiplier = bonusMultiplier;
+        BonusMultiplier = BonusColumns == 0 ? (byte)1 : bonusMultiplier;
         TotalSquares = (byte)(columns * rows);
         BonusSkipChar = bonusSkipChar;
         RowLabels = Label.Rows[..rows];
