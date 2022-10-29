959ba908b105828905dd4c0043e2ff0fdc7d5857
Rolo <roloedits@gmail.com>
2022-10-28 21:03:06 -0700

    updated and added tests

--- Bingo.Core.Tests/GameTest.cs
+++ Bingo.Core.Tests/GameTest.cs
@@ -5,72 +5,62 @@ public sealed class GameTest
     [Fact]
     public void CalculatePlayerScoreTest_Max450()
     {
-        const string key = "YYYYYYYYYYYY";
-        const string guess = "YYYYYYYYYYYY";
-
+        // Arrange
+        var input = "YYYYYYYYYYYY";
         var card = new Card(4, 3, 10, 20, 1, 2);
-        var settings = new Settings()
-        {
-            WillLogStats = true,
-            WillCountAllSameGuessersInStats = true
-        };
+        var settings = new Settings();
+        var player = new Dictionary<string, string>() { { "Rolo", input } };
 
-        var player = new Dictionary<string, string>() { { "Rolo", guess } };
+        var game = new Game(input, card, settings);
 
-        var game = new Game(key, card, settings, player);
+        // Act
+        game.AddPlayers(player);
+        game.Play();
 
-        game.CalculateScore(game.Players[0]);
-
-        var result = game.Players[0].Score;
-
-        Assert.Equal(450, result);
+        // Assert
+        Assert.Equal(450, game.Players[0].Score);
     }
 
     [Fact]
     public void CalculatePlayerScoreTest_Min450()
     {
+        //Arrange
         const string key = "YYYYYYYYYYYY";
         const string guess = "NNNNNNNNNNNN";
 
         var card = new Card(4, 3, 10, 20, 1, 2);
-        var settings = new Settings()
-        {
-            WillLogStats = true,
-            WillCountAllSameGuessersInStats = true
-        };
-
+        var settings = new Settings();
         var player = new Dictionary<string, string>() { { "Rolo", guess } };
+        var game = new Game(key, card, settings);
 
-        var game = new Game(key, card, settings, player);
+        // Act
+        game.AddPlayers(player);
+        game.Play();
 
-        game.CalculateScore(game.Players[0]);
-
-        var result = game.Players[0].Score;
-
-        Assert.Equal(-450, result);
+        // Assert
+        Assert.Equal(-450, game.Players[0].Score);
     }
 
     [Fact]
     public void CalculatePlayerScoreTest_ShouldEqual10_WhenOnlyFreeSpaceIsGiven()
     {
+        // Arrange
         const string key = "YYYYYYYYYYYYYYYYYYYYYYYYY";
         const string guess = "PPPPPPPPPPPPYPPPPPPPPPPPP";
 
-        var card = new Card(5, 5, 10, 0, 5, 1);
+        var card = new Card(5, 5, 10, 0, 0);
         var settings = new Settings()
         {
-            WillLogStats = true,
-            WillCountAllSameGuessersInStats = true
+            AllowSkippingWhenThereIsNoBonus = true,
         };
-
         var player = new Dictionary<string, string>() { { "Rolo", guess } };
+        var game = new Game(key, card, settings);
 
-        var game = new Game(key, card, settings, player);
+        // Act
+        game.AddPlayers(player);
+        game.Play();
 
-        game.CalculateScore(game.Players[0]);
-
-        var result = game.Players[0].Score;
-
-        Assert.Equal(10, result);
+        // Assert
+        Assert.Equal(10, game.Players[0].Score);
     }
 }
---
+++ Bingo.Core.Tests/StatsTest.cs
@@ -0,0 +1,179 @@
+using NuGet.Frameworks;
+using Xunit.Sdk;
+
+namespace Bingo.Core.Tests;
+
+public sealed class StatsTest
+{
+    [Fact]
+    public void MaxScoreShouldBe450()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var settings = new Settings();
+        var player = new Dictionary<string, string>() { { "Rolo", input } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        Assert.Equal(450, game.Stats.MaxScorePossible);
+    }
+
+    [Fact]
+    public void PlayerCountShouldBeOne()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var settings = new Settings();
+        var player = new Dictionary<string, string>() { { "Rolo", input } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        Assert.Equal(1, game.Stats.PlayerCount);
+    }
+
+    [Fact]
+    public void PlayerScores_ShouldEqual_1Count_450Score()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var settings = new Settings();
+        var player = new Dictionary<string, string>() { { "Rolo", input } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        Assert.Equal(450, game.Stats.PlayerScores[0]);
+        Assert.Single(game.Stats.PlayerScores);
+    }
+
+    [Fact]
+    public void PlayerScoreFreq_ShouldEqual_1CountOf450Score()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var settings = new Settings();
+        var player = new Dictionary<string, string>() { { "Rolo", input } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        Assert.Equal(1L, game.Stats.PlayerScoreFrequency[450]);
+        Assert.Equal(1L, game.Stats.FullScoreFrequency[450]);
+    }
+
+    [Fact]
+    public void PercentageShouldBe100Percent()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var settings = new Settings(){ WillCountAllSameGuessersInStats = true};
+        var player = new Dictionary<string, string>() { { "Rolo", input } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        foreach (var percentage in game.Stats.CorrectGuessesPercentage)
+        {
+            Assert.Equal(1.0, percentage);
+            Assert.Equal("100.00%", $"{percentage:P2}");
+        }
+    }
+
+    [Fact]
+    public void PercentageShouldBe0Percent()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 1, 2);
+        var settings = new Settings(){ WillCountAllSameGuessersInStats = true};
+        var player = new Dictionary<string, string>() { { "Rolo", "NNNNNNNNNNNN" } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        foreach (var percentage in game.Stats.CorrectGuessesPercentage)
+        {
+            Assert.Equal(0.0, percentage);
+            Assert.Equal("0.00%", $"{percentage:P2}");
+        }
+    }
+
+    [Fact]
+    public void Have12Skips()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 4, 2);
+        var settings = new Settings(){ WillCountAllSameGuessersInStats = true};
+        var player = new Dictionary<string, string>() { { "Rolo", "PPPPPPPPPPPP" } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        foreach (var data in game.Stats.Skipped)
+        {
+            var skipped = data.Value.Count;
+
+            Assert.Equal(1, skipped);
+        }
+    }
+
+    [Fact]
+    public void ShouldBeAllWrong()
+    {
+        // Arrange
+        const string input = "YYYYYYYYYYYY";
+        var card = new Card(4, 3, 10, 20, 4, 2);
+        var settings = new Settings(){ WillCountAllSameGuessersInStats = true};
+        var player = new Dictionary<string, string>() { { "Rolo", "NNNNNNNNNNNN" } };
+
+        var game = new Game(input, card, settings);
+
+        // Act
+        game.AddPlayers(player);
+        game.Play();
+
+        // Assert
+        foreach (var data in game.Stats.IncorrectGuesses)
+        {
+            var wrong = data.Value.Count;
+
+            Assert.Equal(1, wrong);
+        }
+    }
+}
